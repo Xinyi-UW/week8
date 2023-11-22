c86c5a9f57811cd1a99d101d7e04110b6d10679b
Xinyi-UW <148395443+Xinyi-UW@users.noreply.github.com>
2023-11-21 23:54:54 -0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,30 +1,26 @@
 # logic.py
 import random
 
-
 class Board:
     def __init__(self):
-        self.board = [[None, None, None], [None, None, None], [None, None, None]]
+        self.board = [[None] * 3 for _ in range(3)]
 
     def print_board(self):
         for row in self.board:
             print(row)
 
     def get_winner(self):
-        for i in range(0, 3):
-            if self.board[i][0] is not None and self.board[i][0] == self.board[i][1] == self.board[i][2]:
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] and self.board[i][0] is not None:
                 return self.board[i][0]
-            if self.board[0][i] is not None and self.board[0][i] == self.board[1][i] == self.board[2][i]:
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] and self.board[0][i] is not None:
                 return self.board[0][i]
-        if self.board[1][1] is not None and (self.board[0][0] == self.board[1][1] == self.board[2][2] or self.board[2][0] == self.board[1][1] == self.board[0][2]):
+        if self.board[1][1] is not None and ((self.board[0][0] == self.board[1][1] == self.board[2][2]) or (self.board[2][0] == self.board[1][1] == self.board[0][2])):
             return self.board[1][1]
         return None
 
     def is_draw(self):
-        for row in self.board:
-            if None in row:
-                return False
-        return True
+        return all(all(cell is not None for cell in row) for row in self.board)
 
 
 class Player:
@@ -58,4 +54,5 @@ class Bot(Player):
         while True:
             x, y = random.randint(0, 2), random.randint(0, 2)
             if board[x][y] is None:
+                return x, y
                 return x, y
